59aef5eaa007afb5d465bd23fc23a25fac925fa7
haRsh-756 <74085635+haRsh-756@users.noreply.github.com>
2023-05-01 12:20:21 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.project4fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.project4fx to javafx.fxml;
-    exports com.example.project4fx;
-}
